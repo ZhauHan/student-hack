b6fb609e46c2c81a63d6431fdc52e2368c375f08
s34030jt <joshua.tan-2@student.manchester.ac.uk>
2024-04-13 12:28:51 +0100

    sphere

--- planet-together/src/app/page.tsx
+++ planet-together/src/app/page.tsx
@@ -4,6 +4,7 @@ import { createRoot } from 'react-dom/client'
 import React, { useRef, useState } from 'react'
 import { Canvas, useFrame, ThreeElements } from '@react-three/fiber'
 
+
 export default function Home() {
   return (
     <main className="bg-zinc-800 h-dvh">
@@ -12,8 +13,7 @@ export default function Home() {
         <ambientLight intensity={Math.PI / 2} />
         <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
         <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
-        <Box position={[-1.2, 0, 0]} />
-        <Box position={[1.2, 0, 0]} />
+        <Sphere position={[0, 0, 0]} />
       </Canvas>,
 
     </main >
@@ -34,6 +34,30 @@ function Box(props: ThreeElements['mesh']) {
       onPointerOver={(event) => hover(true)}
       onPointerOut={(event) => hover(false)}>
       <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+    </mesh>
+  )
+}
+
+function Sphere(props: ThreeElements['mesh']) {
+
+  const ref = useRef<THREE.Mesh>(null!)
+  const [hovered, hover] = useState(false)
+  const [clicked, click] = useState(false)
+  useFrame((state, delta) => {
+    ref.current.rotation.x += delta
+    ref.current.rotation.y += delta
+  }
+  )
+  return (
+    <mesh
+      {...props}
+      ref={ref}
+      scale={clicked ? 1.5 : 1}
+      onClick={(event) => click(!clicked)}
+      onPointerOver={(event) => hover(true)}
+      onPointerOut={(event) => hover(false)}>
+      <sphereGeometry args={[1]} />
       <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
     </mesh>
   )
