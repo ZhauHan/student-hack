5c0159793f8e133d17806b5cf5be4958be962397
j36003zc <zhauhan.chi@student.manchester.ac.uk>
2024-04-13 19:02:08 +0100

    add star and sidebar

--- planet-together/src/app/page.tsx
+++ planet-together/src/app/page.tsx
@@ -5,18 +5,24 @@ import React, { useRef, useState } from 'react'
 import { Canvas, useFrame, ThreeElements } from '@react-three/fiber'
 import { OrbitControls } from '@react-three/drei'
 import Planet from './components/Planet'
+import Sidebar from './components/Sidebar'
+import Stars from './components/Stars'
 
 export default function Home() {
   return (
-    <main className="bg-zinc-800 h-dvh">
-      <div className="text-amber-200 p-5 text-xl font-bold">Welcome to Planet Together!</div>
-      <Canvas>
-        <ambientLight intensity={Math.PI / 2} />
-        <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
-        <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
-        <Planet position={[0, 0, 0]} />
-        <OrbitControls />
-      </Canvas>,
+    <main className="bg-zinc-800 h-screen">
+      
+      <div className='flex h-screen'>
+        <Sidebar />
+        <Canvas>
+          <ambientLight intensity={Math.PI / 2} />
+          <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
+          <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
+          <Planet position={[0, 0, 0]} />
+          <OrbitControls />
+          <Stars />
+        </Canvas>,
+      </div>
     </main >
   );
 }
