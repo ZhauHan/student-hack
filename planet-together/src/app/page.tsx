9f9eeb7fb4b06b03f8ee9263e2eda099fcd6820c
j36003zc <zhauhan.chi@student.manchester.ac.uk>
2024-04-13 12:23:56 +0100

    allow change scene with dragging

--- planet-together/src/app/page.tsx
+++ planet-together/src/app/page.tsx
@@ -3,6 +3,7 @@ import * as THREE from 'three'
 import { createRoot } from 'react-dom/client'
 import React, { useRef, useState } from 'react'
 import { Canvas, useFrame, ThreeElements } from '@react-three/fiber'
+import { OrbitControls } from '@react-three/drei'
 
 export default function Home() {
   return (
@@ -14,6 +15,7 @@ export default function Home() {
         <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
         <Box position={[-1.2, 0, 0]} />
         <Box position={[1.2, 0, 0]} />
+        <OrbitControls />
       </Canvas>,
 
     </main >
