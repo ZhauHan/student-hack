ad0cdb10de0350bf875799f1dd89e8e8d4583489
Samuel Goh Jun Je <samuel.goh@student.manchester.ac.uk>
2024-04-13 22:36:18 +0100

    Added function to add planets

--- planet-together/src/app/components/Planet.tsx
+++ planet-together/src/app/components/Planet.tsx
@@ -1,10 +1,17 @@
 "use client"
 import * as THREE from 'three'
-import React, { useRef, useState } from 'react'
+import React, { useRef, useState, useEffect } from 'react'
 import { Canvas, useFrame, ThreeElements } from '@react-three/fiber'
+import { Html } from "@react-three/drei"
 
-function Planet(props: ThreeElements['mesh']) {
-
+function Planet(props: {
+  texture: string, 
+  mesh: ThreeElements['mesh'], 
+  position: [number, number, number], 
+  onClick: () => void, 
+  planetCount: number,
+  setPlanetCount: React.Dispatch<React.SetStateAction<number>>
+}) {
     const ref = useRef<THREE.Mesh>(null!)
     const [hovered, hover] = useState(false)
     const [clicked, click] = useState(false)
@@ -15,15 +22,15 @@ function Planet(props: ThreeElements['mesh']) {
     }
     )
 
-    const texture = new THREE.TextureLoader().load('images/ear0xuu2.jpg'); 
-  
+    const texture = new THREE.TextureLoader().load(props.texture);
+
     return (
 
       <mesh
         {...props}
         ref={ref}
         scale={1}
-        onClick={(event) => click(!clicked)}
+        onClick={props.onClick}
         onPointerOver={(event) => hover(true)}
         onPointerOut={(event) => hover(false)}>
         <sphereGeometry args={[1]} />
@@ -33,4 +40,47 @@ function Planet(props: ThreeElements['mesh']) {
     )
   }
 
-export default Planet;
+export default function ThreePlanets(props: {
+  planetCount: number,
+  setPlanetCount: React.Dispatch<React.SetStateAction<number>>
+  visiblePlanets: { texture: string, isVisible: boolean }[]
+  setVisiblePlanets: React.Dispatch<React.SetStateAction<{ texture: string, isVisible: boolean }[]>>
+}) {
+
+  // const [planetCount, setPlanetCount] = useState(3);
+
+  const handleAddPlanet = (planetTexture: string) => {
+    props.setVisiblePlanets([...props.visiblePlanets, {texture: planetTexture, isVisible: true}]);
+    props.setPlanetCount(prevPlanetCount => prevPlanetCount + 1);
+    console.log("Planet count: ", props.planetCount);
+  };
+  
+  // const handleClick = (index: number) =>  {
+  //   setVisiblePlanets(prevVisiblePlanets => {
+  //       const newVisiblePlanets = [...prevVisiblePlanets];
+  //       newVisiblePlanets[index].isVisible = false;
+  //       setPlanetCount(planetCount => planetCount - 1);
+  //       return newVisiblePlanets;
+  //   });
+  //   setPlanetCount(planetCount => planetCount - 1);
+  //   console.log("Planet count: ", planetCount);
+  // };
+
+  return (
+  <>
+    {/* <div>Number of planets: {planetCount}</div> */}
+     {props.visiblePlanets.map((planet, index) => 
+      planet.isVisible && 
+      <Planet 
+        key={index} 
+        mesh={{}} 
+        position={[index * 10, 0, 0]} 
+        texture={planet.texture} 
+        onClick={() => handleAddPlanet(planet.texture)} 
+        planetCount={props.planetCount} 
+        setPlanetCount={props.setPlanetCount}
+      />
+    )}
+  </>
+  );
+}
--- planet-together/src/app/page.tsx
+++ planet-together/src/app/page.tsx
@@ -5,21 +5,59 @@ import React, { useRef, useState } from 'react'
 import { Canvas, useFrame, ThreeElements } from '@react-three/fiber'
 import { OrbitControls } from '@react-three/drei'
 import Planet from './components/Planet'
+import ThreePlanets from './components/Planet'
 
 export default function Home() {
+  const [visiblePlanets, setVisiblePlanets] = useState([
+    { texture: 'images/sat0fds1.jpg', isVisible: true },
+    { texture: 'images/mar2kuu2.jpg', isVisible: true },
+    { texture: 'images/ear0xuu2.jpg', isVisible: true },
+  ]);
+  const [planetCount, setPlanetCount] = useState(3);
+
+  // const handleAddPlanet = () => {
+  //   const newPlanets = [];
+  //   const numberOfNewPlanets = 1; // Change this to the number of planets you want to add
+
+  //   for (let i = 0; i < numberOfNewPlanets; i++) {
+  //     newPlanets.push(true);
+  //   }
+  //   setVisiblePlanets(prevVisiblePlanets => [...prevVisiblePlanets, true]);
+  //   setPlanetCount(planetCount => planetCount + 1);
+  // };
+
+  // const handleClick = (index: number) => (event: React.MouseEvent<HTMLButtonElement>) => {
+  //   setVisiblePlanets(prevVisiblePlanets => {
+  //     const newVisiblePlanets = [...prevVisiblePlanets];
+  //     newVisiblePlanets[index] = false;
+  //     return newVisiblePlanets;
+  //   });
+  //   setPlanetCount(planetCount => planetCount - 1);
+  // };
+
+
+  // // button1 click event listener to apply function
+  // button1.addEventListener("click", function() {}
+  
+
   return (
     <main className="bg-zinc-800 h-dvh">
       <div className="text-amber-200 p-5 text-xl font-bold">Welcome to Planet Together!</div>
+      <div>
+        <p style={{ color: 'red' }}> Current planet count: {planetCount}</p>
+      </div>
+
       <Canvas>
         <ambientLight intensity={Math.PI / 2} />
         <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
         <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
-        <Planet position={[0, 0, 0]} />
+        <ThreePlanets setPlanetCount={setPlanetCount} planetCount={planetCount} setVisiblePlanets={setVisiblePlanets} visiblePlanets={visiblePlanets}/>
         <OrbitControls />
       </Canvas>,
     </main >
   );
 }
+
 
 
 
