497eacada4be6933949fb30f9a0673324526eafb
s34030jt <joshua.tan-2@student.manchester.ac.uk>
2024-04-14 05:48:20 +0100

    hotfixed something

--- planet-together/src/app/page.tsx
+++ planet-together/src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 import * as THREE from 'three'
 import { createRoot } from 'react-dom/client'
-import React, { useRef, useState, useEffect } from 'react'
+import React, { useRef, useState, useEffect, createRef } from 'react'
 import { Canvas, useFrame, useThree } from '@react-three/fiber'
 import { OrbitControls, Plane } from '@react-three/drei'
 
@@ -10,6 +10,7 @@ import Sidebar from './components/Sidebar'
 import Stars from './components/Stars'
 import SpeedScrollBar from './components/SpeedScrollBar'
 import { SliderValue } from '@nextui-org/react'
+import { randFloat } from 'three/src/math/MathUtils.js'
 export interface PlanetData {
   planetName: String
   position: THREE.Vector3,
@@ -20,34 +21,55 @@ export interface PlanetData {
   show: boolean,
   planets: PlanetData[],
   setPlanets: React.Dispatch<React.SetStateAction<PlanetData[]>>,
-  ref: React.MutableRefObject<THREE.Mesh<THREE.BufferGeometry<THREE.NormalBufferAttributes>, THREE.Material | THREE.Material[], THREE.Object3DEventMap>>,
+  ref: React.RefObject<THREE.Mesh<THREE.BufferGeometry<THREE.NormalBufferAttributes>, THREE.Material | THREE.Material[], THREE.Object3DEventMap>>,
 }
 
-const MovingGrid = () => {
+
+
+const MovingGrid = (props: { planets:PlanetData[], setPlanets: React.Dispatch<React.SetStateAction<PlanetData[]>>, isAdding: boolean }) => {
   const gridRef = useRef<THREE.GridHelper>(null)
 
   const { raycaster, mouse, camera, scene } = useThree()
 
-  useEffect(() => {
-    const onClick = (event: MouseEvent) => {
-      raycaster.setFromCamera(mouse, camera)
-      const intersects = raycaster.intersectObject(gridRef.current!)
-      if (intersects.length > 0) {
-        const intersect = intersects[0]
-        console.log(intersect.point)
+  const handleAddRandomPlanet = (position:THREE.Vector3) => {
+    const randomPlanet:PlanetData = {
+      planetName: "planet" + props.planets.length,
+      position: position,
+      mass: randFloat(3e15, 9e24),
+      velocity: new THREE.Vector3(randFloat(-3000, 3000), randFloat(-3000, 3000), randFloat(-3000, 3000)),
+      texture: 'images/ear0xuu2.jpg',
+      radius: randFloat(0.1, 1),
+      show: true,
+      planets: props.planets,
+      setPlanets: props.setPlanets,
+      ref: createRef<THREE.Mesh>(),
+    }
+
+    props.setPlanets([...props.planets, randomPlanet])
+  };
+
+  const onClick = (event: MouseEvent) => {
+    raycaster.setFromCamera(mouse, camera)
+    const intersects = raycaster.intersectObject(gridRef.current!)
+    if (intersects.length > 0) {
+      const intersect = intersects[0]
+      if (props.isAdding) {
+        handleAddRandomPlanet(new THREE.Vector3(intersect.point.x, intersect.point.y, intersect.point.z))
       }
     }
+  }
+
+
     const canvas = document.querySelector('canvas')
     if (canvas) {
       canvas.addEventListener('mousedown', onClick)
     }
-    // Clean up the event listener on unmount
-    return () => {
-      if (canvas) {
-        canvas.removeEventListener('mousedown', onClick)
-      }
-    }
-  } , [raycaster, mouse, camera])
+    // // Clean up the event listener on unmount
+    // return () => {
+    //   if (canvas) {
+    //     canvas.removeEventListener('mousedown', onClick)
+    //   }
+    // }
 
 
   useFrame(({ camera }) => {
@@ -100,6 +122,8 @@ export default function Home() {
 
   const [updateFreq, setUpdateFreq] = useState<SliderValue>(0.1)
 
+  const [isAdding, setIsAdding] = useState<boolean>(false)
+
   const stars = React.useMemo( () => <Stars />, [])
   useEffect(() => {
     setPlanetCount(planets.length);
@@ -111,7 +135,7 @@ export default function Home() {
     <main className="bg-zinc-800 h-screen">
       <div className="flex h-full">
 
-        <Sidebar setIsOrbit={ setIsOrbit } isOrbit={ isOrbit } planetCount={ planetCount }/>
+        <Sidebar setIsOrbit={ setIsOrbit } isOrbit={ isOrbit } planetCount={ planetCount } setIsAdding={ setIsAdding } isAdding={ isAdding }/>
         <div className="flex flex-col grow h-full">
           <Canvas className="w-max h-max">
             <ambientLight intensity={Math.PI / 2} />
@@ -120,6 +144,7 @@ export default function Home() {
             <System setPlanets={setPlanets} planets={planets} updateFreq={updateFreq}/>
             <OrbitControls zoom0={0.5} enabled={isOrbit}/>
             { stars }
+            <MovingGrid planets={planets} setPlanets={setPlanets} isAdding={ isAdding }/>
           </Canvas>
           <div className="flex justify-center">
             <SpeedScrollBar setUpdateFreq={setUpdateFreq} />
