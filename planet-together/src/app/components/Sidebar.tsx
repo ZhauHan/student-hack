9223b1669e6489068485ff14fb5801a95352c030
s34030jt <joshua.tan-2@student.manchester.ac.uk>
2024-04-14 07:34:09 +0100

    minor changes

--- planet-together/src/app/components/Sidebar.tsx
+++ planet-together/src/app/components/Sidebar.tsx
@@ -25,18 +25,6 @@ function Card({ title , description, image } : CardProps) {
   );
 }
 
-function Sidebar(props: {setIsOrbit: React.Dispatch<React.SetStateAction<boolean>>, isOrbit: boolean, planetCount: number, setIsAdding: React.Dispatch<React.SetStateAction<boolean>>, isAdding: boolean }) {
-  return (
-    <div className="sidebar w-160 border border-black flex flex-col overflow-y-auto scrollbar-thin scrollbar-thumb-gray-500 scrollbar-track-gray-200">
-      <div className="text-amber-200 p-5 text-xl font-bold">Planet Together</div>
-      <div className="text-amber-200 p-5 text-xl font-bold">Planet Count: {props.planetCount}</div>
-      <button className="m-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700 active:bg-blue-800 focus:outline-none" onClick={() => { props.setIsOrbit(!props.isOrbit) }}>Change Camera Mode</button>
-      <button className="m-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700 active:bg-blue-800 focus:outline-none" onClick={() => { props.setIsAdding(!props.isAdding) }}>Add Planet Mode</button>
-      <Card title="Moon" description="This is the first card." image="images/moon.jpg" />
-      <Card title="Mars" description="This is the second card." image="images/mars.jpg" />
-    </div>
-  );
-}
 
 const planets: CardProps[] = [
   { title: "Venus", description: "This is the first card.", image: "images/ven0aaa2.jpg" },
@@ -72,11 +60,13 @@ const customStyles = {
 };
 
 
-export default function Sidebar(props: {setIsOrbit: (isOrbit: boolean) => void, isOrbit: boolean , planetCount: number, visiblePlanets: { name: string, texture: string, isVisible: boolean }[] }) {
+export default function Sidebar(props: {setIsOrbit: React.Dispatch<React.SetStateAction<boolean>>, isOrbit: boolean, planetCount: number, setIsAdding: React.Dispatch<React.SetStateAction<boolean>>, isAdding: boolean , visiblePlanets: { name: string, texture: string, isVisible: boolean }[] }) {
   return (
     <div className="sidebar w-200 border border-black flex flex-col overflow-y-auto scrollbar-thin scrollbar-thumb-gray-500 scrollbar-track-gray-200">
       <div className="text-amber-200 p-5 text-xl font-bold">Planet Together</div>
-      <button className="m-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700 active:bg-blue-800 focus:outline-none" onClick={() => { props.setIsOrbit }}>Change Camera Mode</button>
+      <div className="text-amber-200 p-5 text-xl font-bold">Planet Count: {props.planetCount}</div>
+      <button className="m-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700 active:bg-blue-800 focus:outline-none" onClick={() => { props.setIsOrbit(!props.isOrbit) }}>Change Camera Mode</button>
+      <button className="m-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700 active:bg-blue-800 focus:outline-none" onClick={() => { props.setIsAdding(!props.isAdding) }}>Add Planet Mode</button>
       <Select 
         options={planets} 
         components={{
@@ -105,6 +95,7 @@ export default function Sidebar(props: {setIsOrbit: (isOrbit: boolean) => void, 
         menu: (provided: any) => ({
           ...provided,
           backgroundColor: 'transparent', 
+          color: 'white'
         }),
       }}      
       isSearchable={false}
